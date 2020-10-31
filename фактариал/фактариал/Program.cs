8059c7110370dd2811ea493a04d090c056fe4ecf
Yaroslav131 <72652633+Yaroslav131@users.noreply.github.com>
2020-10-31 19:43:47 +0300

    no message

---
+++ фактариал/фактариал/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+
+namespace фактариал
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ulong n;
+            ulong i;
+         
+            double f = 1;
+            ulong schet = 0;
+
+            while (!ulong.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Write number corectly");
+            }
+           
+         
+            //счетчик
+            for ( i = 1; i <= n; i++)
+            {
+                f *= i;
+
+
+                while (f >= 1)
+                {
+                    schet++;
+                    f /= 10;
+                }
+
+            }
+           
+            //масив фактариала
+            ulong[] arr_fack = new ulong[schet];
+            ulong j;
+            arr_fack[schet-1] = 1;
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 0; j < schet; j++)
+                {
+                    arr_fack[j] *= i;
+                }
+                for (j = 0; j < schet; j++)
+                {
+                    while (arr_fack[schet - 1-j] >= 10)
+                    {
+                        arr_fack[schet - 2 - j] += arr_fack[schet - 1 - j] / 10;
+                        arr_fack[schet - 1 - j] = arr_fack[schet - 1 - j] % 10;
+                    }
+                }
+
+            }
+
+           
+            Console.WriteLine("Вывод факториала: ");
+            for (j = 0; j < schet; j++)
+            {
+                Console.Write($"{arr_fack[j]} ");
+            }
+
+
+        }
+    }
+}
