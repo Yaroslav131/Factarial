da2eddd598e2e0869889fdfdea4af6f44159411a
Yaroslav131 <72652633+Yaroslav131@users.noreply.github.com>
2020-11-05 11:32:10 +0300

    изменил названия переменных

--- фактариал/фактариал/Program.cs
+++ фактариал/фактариал/Program.cs
@@ -7,62 +7,49 @@ namespace фактариал
     {
         static void Main(string[] args)
         {
-
             ulong n;
             ulong i;
-         
-            double f = 1;
+            double fack = 1;
             ulong schet = 0;
 
             while (!ulong.TryParse(Console.ReadLine(), out n))
             {
-                Console.WriteLine("Write number corectly");
+                Console.WriteLine("Write number correctly");
             }
-           
-         
             //счетчик
             for ( i = 1; i <= n; i++)
             {
-                f *= i;
-
-
-                while (f >= 1)
+                fack *= i;
+                while (fack >= 1)
                 {
                     schet++;
-                    f /= 10;
+                    fack /= 10;
                 }
-
             }
-           
             //масив фактариала
-            ulong[] arr_fack = new ulong[schet];
+            ulong[] arrFack = new ulong[schet];
             ulong j;
-            arr_fack[schet-1] = 1;
+            arrFack[schet-1] = 1;
             for (i = 1; i <= n; i++)
             {
                 for (j = 0; j < schet; j++)
                 {
-                    arr_fack[j] *= i;
+                    arrFack[j] *= i;
                 }
                 for (j = 0; j < schet; j++)
                 {
-                    while (arr_fack[schet - 1-j] >= 10)
+                    while (arrFack[schet - 1-j] >= 10)
                     {
-                        arr_fack[schet - 2 - j] += arr_fack[schet - 1 - j] / 10;
-                        arr_fack[schet - 1 - j] = arr_fack[schet - 1 - j] % 10;
+                        arrFack[schet - 2 - j] += arrFack[schet - 1 - j] / 10;
+                        arrFack[schet - 1 - j] = arrFack[schet - 1 - j] % 10;
                     }
                 }
-
-            }
-
-           
+            }           
             Console.WriteLine("Вывод факториала: ");
             for (j = 0; j < schet; j++)
             {
-                Console.Write($"{arr_fack[j]} ");
+                Console.Write($"{arrFack[j]} ");
             }
-
-
         }
     }
 }
